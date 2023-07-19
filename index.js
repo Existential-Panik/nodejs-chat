80d9d641708ab6c7da14a80613671c614427608e
Existential-Panik <sthasuyash24@gmail.com>
2023-07-19 14:44:50 +0545

    maybe now it works, even more restoration

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("layout");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 const users = {};
